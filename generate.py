0fda85cb6dade71716212e4a950b2936269b4168
Carlos <26363920+Shandelier@users.noreply.github.com>
2021-01-19 01:31:53 +0100

    init synthetic dataset benchmark

---
+++ generate.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import preprocess_utils as pre
+import numpy as np
+
+from sklearn.datasets import make_classification
+
+
+def gen_data(n_samples, n_features, n_classes, weights):
+    return make_classification(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_classes=n_classes,
+        n_repeated=0,
+        n_redundant=0,
+        n_informative=3,
+        flip_y=.05,
+        random_state=420,
+        n_clusters_per_class=2,
+        weights=weights,
+    )
+
+
+datasets = r"./datasets"
+
+n_samples = 100
+n_features = [100, 200, 300, 500, 1000, 2000, 5000]
+n_classes = 2
+print("2 balanced class")
+for f in n_features:
+    X, y = gen_data(n_samples, n_features, n_classes, 1)
+    ds = np.concatenate((X, y.T), axis=1)
+    np.savetxt(datasets+"/2-class-balanced_{}_atr.csv".format(f), delimiter=",")
+
+n_features = 50
+n_classes = 2
+ratio = {"1-9": [.1, .9],
+         "2-8": [.2, .8],
+         "3-7": [.3, .7],
+         "4-6": [.4, .6],
+         "5-5": [.5, .5]}
+print("2 class")
+for f in n_features:
+    X, y = gen_data(n_samples, n_features, n_classes, ratio[f])
+    ds = np.concatenate((X, y.T), axis=1)
+    np.savetxt(datasets+"/2_class_{}_ratio.csv".format(f), delimiter=",")
+
+n_classes = 3
+n_features = 50
+ratio = {"[7-2-1]": [.7, .2, .1],
+         "[6-3-1]": [.6, .3, .1],
+         "[5-3-2]": [.5, .3, .2],
+         "[4-4-2]": [.4, .4, .2],
+         "[3-3-3]": [.33, .33, .33]}
+print("3 class")
+for f in n_features:
+    X, y = gen_data(n_samples, n_features, n_classes, ratio[f])
+    ds = np.concatenate((X, y.T), axis=1)
+    np.savetxt(datasets+"/2_class_{}_ratio.csv".format(f), delimiter=",")
